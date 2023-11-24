73afd03125ccf1be1995ed12460fc0796435f5af
NakerTheFirst <kcholysk@gmail.com>
2023-11-25 00:52:35 +0100

    Add dft method

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
+import numpy as np
+
+
 class FFT:
-    def __init__(self, signal):
-        self.__signal = signal
+    def __init__(self):
         self.__data = []
         self.__data_noisy = []
         self.__data_2d = []
 
-    def scrape_data(self):
+    def run(self):
+        self.__scrape_data()
+
+        data_dft = self.__dft(self.__data)
+        print(f"data_dft: {data_dft}")
+
+    def __scrape_data(self):
         files = ["dane_02.in", "dane_02_a.in", "dane2_02.in"]
         lists = [self.__data, self.__data_noisy, self.__data_2d]
 
@@ -17,12 +25,26 @@ class FFT:
                 for line in lines[2:]:
                     row_data = list(map(float, line.split()))
                     data_list.append(row_data)
-            else:
-                for line in lines:
-                    try:
-                        data_list.append(float(line))
-                    except ValueError:
-                        print(f"Could not convert {line} to float")
+                continue
+
+            for line in lines[2:]:
+                try:
+                    data_list.append(float(line))
+                except ValueError:
+                    print(f"Could not convert {line} to float")
+
+    def __dft(self, x):
+        N = len(x)
+        X = np.zeros(N, dtype=complex)
+
+        for k in range(N):
+            for n in range(N):
+                X[k] += x[n] * np.exp(-2j * np.pi * k * n / N)
+
+        return X
+
+    def __count_operations(self):
+        pass
 
     def get_data(self):
         return self.__data
@@ -36,9 +58,8 @@ class FFT:
 
 def main():
 
-    data = []
-    f = FFT([1, 2, 3])
-    f.scrape_data()
+    f = FFT()
+    f.run()
 
     print(f"Data: {f.get_data()}")
     print(f"Data noise: {f.get_data_noisy()}")
